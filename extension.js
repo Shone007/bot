08a3a4496c09fae1c2648dbc4b065e7cc2880dba
Shone007 <bojanicneso@gmail.com>
2015-02-12 19:59:08 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -108,6 +108,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
+    $.getScript('https://rawgit.com/Shone007/bot/master/script.js', extend);
 
 }).call(this);
